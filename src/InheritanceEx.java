550af9aeeb9db89a2e1c3cc0c0f4c1843e873d5b
임소영 <s2412@e-mirim.hs.kr>
2025-03-09 16:07:40 +0900

    상속 / 예제 1-2

--- src/InheritanceEx.java
+++ src/InheritanceEx.java
@@ -21,16 +21,11 @@ class Student extends Person {
         //weight = 65 -> private 때문에 에러 발생
         setWeight(65); //반환 메소드로 간접접근
     }
-
-    public void show1(){
-        System.out.println(age);
-    }
 }
 
 public class InheritanceEx {
     public static void main(String[] args) {
         Student s = new Student();
-        s.show1();
         s.set();
     }
 }
